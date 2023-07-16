398607dc0b3704729a76a6a35e095ba3c3c6d7ee
alex <alexfasmod@mail.ru>
2023-07-16 13:35:13 +0500

    fix

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
 #include "uart.h"
+#include "wifi.h"
 
-// static const char *TAG = "MAIN";
+static const char *TAG = "MAIN";
 // static esp_err_t error;
 
 void app_main() {
     uart_init();
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init();
     xTaskCreate(rx_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
     xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-2, NULL);
 }
